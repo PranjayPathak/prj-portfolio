d460180b3308cd642a0f47d142349ddd6e21499c
pranjaypathak411 <pranjaypathak411@gmail.com>
2022-11-07 02:47:24 +0530

    Complete hero section and add Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
-import Logo from './Logo';
-import { FaSun, FaMoon, FaMusic } from "react-icons/fa";
+import Logo from './common/Logo';
+// import Link from 'next/link';
+// import { FaSun, FaMoon, FaMusic } from "react-icons/fa";
 
 type Props = {}
 
 function Header({ }: Props) {
   return (
-    <header  className='top-navbar'>
+    <header className='top-navbar'>
       <Logo />
 
       <div className='top-navbar__items'>
-        <div className='top-navbar__items__item'><FaMusic/></div>
-        <div className='top-navbar__items__item'><FaMoon/></div>
-        <div className='top-navbar__items__item'>Contact me</div>
+        {/* <div className='top-navbar__items__item'><FaMusic/></div>
+        <div className='top-navbar__items__item'><FaMoon/></div> */}
+        <a href='#contact'>
+          <div className='top-navbar__items__item contact-link label-2'>
+            CONTACT ME
+          </div>
+        </a>
       </div>
       {/* Logo  */}
       {/* Contact me */}
--- components/LeftMenu.tsx
+++ components/LeftMenu.tsx
@@ -1,40 +1,44 @@
-import React from 'react'
-
-type Props = {}
+import React from "react";
+// import Link from "next/link";
+type Props = {};
 
 function LeftMenu({ }: Props) {
-    const items = [
-        {
-            label: 'About',
-            link: '#'
-        },
-        {
-            label: 'Skills',
-            link: '#'
-        },
-        {
-            label: 'Experience',
-            link: '#'
-        },
-        {
-            label: 'Projects',
-            link: '#'
-        },
-    ]
-    return (
-        <div className='left-navigation'>
-            {
-                items.map((item) => {
-                    return (
-                        <div className='left-navigation__item' key={item.label} >
-                            <div className='left-navigation__item__bar'></div>
-                            {item.label}
-                        </div>
-                    )
-                })
-            }
-        </div >
-    )
+  const items = [
+    {
+      label: "About",
+      link: "#about",
+    },
+    {
+      label: "Experience",
+      link: "#experience",
+    },
+    {
+      label: "Skills",
+      link: "#skills",
+    },
+    {
+      label: "Projects",
+      link: "#projects",
+    },
+  ];
+  return (
+    <div className="left-navigation">
+      {items.map((item) => {
+        return (
+          <a href={item.link} className="left-navigation__item" key={item.label}>
+            {/* <a href={item.link}> */}
+              <div className="bar"></div>
+              {/* <Link> */}
+              <span className="link-item label-2">
+                {item.label}
+              </span>
+              {/* </Link> */}
+            </a>
+          // </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default LeftMenu
+export default LeftMenu;
