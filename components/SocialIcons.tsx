4b5f40fc4f8cf655f51da13378bbb3c9b8420f10
pranjaypathak411 <pranjaypathak411@gmail.com>
2022-11-07 02:55:27 +0530

    Add base CSS and logo

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -1,38 +1,60 @@
-import Image from 'next/image';
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 // import { SocialIcon } from 'react-social-icons';
+import { motion } from "framer-motion";
+import { IconContext } from "react-icons";
 import { FaGithubSquare, FaLinkedin, FaInstagramSquare } from "react-icons/fa";
-type Props = {}
+type Props = {};
 
-function SocialIcons({ }: Props) {
+function SocialIcons({}: Props) {
+  const iconList = [
+    {
+      id: 1,
+      label: "Linkedin",
+      icon: <FaLinkedin />,
+      link: "https://www.linkedin.com/in/pranjaypathak/",
+    },
+    {
+      id: 2,
+      label: "Github",
+      icon: <FaGithubSquare />,
+      link: "https://github.com/PranjayPathak",
+    },
+    {
+      id: 3,
+      label: "Instagram",
+      icon: <FaInstagramSquare />,
+      link: "https://www.instagram.com/pranjay_pathak/?hl=en",
+    },
+  ];
 
-    // const links = [
-    //     {
-    //         id:1,
-    //         label:'Linkedin',
-    //         icon: BsLinkedin,
-    //         link: "https://www.linkedin.com/in/pranjaypathak/"
-    //     },
-    //     {
-    //         id:2,
-    //         label:'Github',
-    //         icon: BsGithub,
-    //         link: "https://github.com/PranjayPathak"
-    //     },
-    //     {
-    //         id:3,
-    //         label:'Instagram',
-    //         icon: BsInstagram,
-    //         link: "https://www.instagram.com/pranjay_pathak/?hl=en"
-    //     },
-    // ]
-    return (
-        <div className='social-icons'>
-            <div className='social-icons__icon' ><FaLinkedin /></div>
-            <div className='social-icons__icon'><FaGithubSquare /></div>
-            <div className='social-icons__icon'><FaInstagramSquare /></div>
-        </div>
-    )
+  return (
+    <IconContext.Provider value={{ className: "social-icons__icon" }}>
+      <div className="social-icons">
+        {iconList.map((item) => {
+          return (
+            <a key={item.id} target="_blank" rel="noopener noreferrer" href={item.link}>
+              <motion.div
+              
+              initial={{ x: -100, y: 100, scale: 0.8 }}
+              animate={{ x: 0, y: 0, scale: 1 }}
+              transition={{ delay: item.id / 10, type:"tween" }}
+            >
+              {item.icon}
+            </motion.div>
+            </a>
+          );
+        })}
+
+        {/* <motion.div>
+          <FaGithubSquare />
+        </motion.div>
+        <motion.div>
+          <FaInstagramSquare />
+        </motion.div> */}
+      </div>
+    </IconContext.Provider>
+  );
 }
 
-export default SocialIcons
+export default SocialIcons;
