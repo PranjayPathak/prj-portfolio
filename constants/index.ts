93af1d4fe94f32f83e80a9165326893b0e1352b4
PRJ <53118772+PranjayPathak@users.noreply.github.com>
2023-12-29 00:55:10 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
 const MAIL_ID = "pranjaypathak411@gmail.com"
-const RESUME_LINK = 'https://drive.google.com/file/d/1oE9b_tv6jfOxPS_RBLbtkCSLSNYUQFaU/view'
+const RESUME_LINK = "https://drive.google.com/file/d/1fkrnKoIeDgyqZsgry95mRASXFltxf1Hm/view"
 export default {MAIL_ID, RESUME_LINK}
